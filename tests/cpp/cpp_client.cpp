e5b95a360e1addbd1016320b09ada914c1686a06
out0 <cristianoo@gmail.com>
2025-11-18 00:14:04 -0300

    Making tests send and receive

--- tests/cpp/cpp_client.cpp
+++ tests/cpp/cpp_client.cpp
@@ -2,19 +2,28 @@
 #include <cstring>
 #include <unistd.h>
 
-#define SIZE 2097100
+#define SIZE 209710000
+#define TIMEOUT 2
 int main()
 {
-    auto link = new Datalink("127.0.0.1", 21001, 2);
+    auto link = new Datalink("127.0.0.1", 21000, TIMEOUT);
     int data_count = 0;
+
+    char *huge_data = new char[SIZE];
+    for (int i = 0; i < SIZE; i++)
+        huge_data[i] = i % 100;
 
     while (true)
     {
-        auto res = link->receiveData();
+        auto res = link->receiveData(TIMEOUT);
         if (res == nullptr)
             continue;
 
-        printf("receiving data %ld data_count: %d\n", res->size, data_count);
+        printf("received data %ld data_count: %d\n", res->size, data_count);
+
+        printf("sending data...\n");
+        link->sendData(huge_data, SIZE);
+
         // if (res->size != 2097100)
         // {
         //     printf("size mismatch\n");
--- tests/cpp/cpp_server.cpp
+++ tests/cpp/cpp_server.cpp
@@ -1,25 +1,37 @@
 #include "../../include/datalink.h"
 #include <unistd.h>
 
-#define SIZE 2097100
+#define SIZE 209710
+#define PORT 21000
+#define TIMEOUT 2
+
 int main()
 {
-    auto link = Datalink(21001, 2);
+
+    auto link = Datalink(PORT, TIMEOUT);
     int data_count = 0;
 
     char *huge_data = new char[SIZE];
     for (int i = 0; i < SIZE; i++)
         huge_data[i] = i % 100;
 
+    
     while (true)
     {
         if (!link.isReady()) {
             printf ("link not ready\n");
-            sleep(1);
-            continue;
+            while (!link.isReady()) {
+                sleep(1);
+                continue;
+            }
         }
         printf("sending data %d\n", data_count);
         link.sendData(huge_data, SIZE);
+
+        auto rcvd = link.receiveData(TIMEOUT);
+
+        if (rcvd == nullptr) continue;
+        printf("receiving data %ld\n", rcvd->size);
 
         data_count++;
         //sleep(1);
