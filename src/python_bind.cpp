e7c3297a5c794cfd11bcf309ce31464d5f990b40
out0 <cristianoo@gmail.com>
2025-12-12 15:52:10 -0300

    migrating python bind to datalink class

--- src/python_bind.cpp
+++ src/python_bind.cpp
@@ -2,7 +2,6 @@
 #include <memory>
 #include <cstring>
 #include "../include/datalink.h"
-#include "../include/tcplink.h"
 
 // #define DEBUG_BIND 1
 
@@ -19,22 +18,28 @@ extern double *data_decode_double(uint8_t *data, long size);
 
 extern "C"
 {
-        void *init_tcp_server(int port, float timeout, bool debug)
+        typedef struct datalink_ptr {
+                std::shared_ptr<Datalink> link;
+        } datalink_ptr;
+
+        void *init_tcp_server(int port, float timeout)
         {
-                auto link = new TCPLink(port, timeout, debug);
+                auto link = Datalink::TcpServer(port, timeout);
 #ifdef DEBUG_BIND
                 printf("creating a Datalink server %p with port: %d\n", link, port);
 #endif
-                return link;
+                datalink_ptr *ptr = new datalink_ptr{link};
+                return ptr;
         }
 
-        void *init_tcp_client(char *host, int port, float timeout, bool debug)
+        void *init_tcp_client(char *host, int port, float timeout)
         {
-                auto link = new TCPLink(host, port, timeout, debug);
+                auto link = Datalink::TcpClient(host, port, timeout);
 #ifdef DEBUG_BIND
                 printf("creating a Datalink client %p to host %s and port: %d\n", link, host, port);
 #endif
-                return link;
+                datalink_ptr *ptr = new datalink_ptr{link};
+                return ptr;
         }
 
         void destroy_tcp_link(void *link)
@@ -42,7 +47,9 @@ extern "C"
 #ifdef DEBUG_BIND
                 printf("destroying the Datalink %p\n", link);
 #endif
-                delete ((TCPLink *)link);
+                auto ptr = ((datalink_ptr *)link);                
+                ptr->link.reset();
+                delete ptr;
         }
 
         bool write_raw_data(void *link, uint8_t *data, long size, double timestamp)
@@ -50,40 +57,41 @@ extern "C"
 #ifdef DEBUG_BIND
                 printf("writing string to the Datalink %p size: %ld\n", link, size);
 #endif
-                return ((TCPLink *)link)->write(data, size, timestamp);
+                return (*(datalink_ptr *)link).link->write(data, size, timestamp);
         }
 
         bool is_ready(void *link)
         {
-                return ((TCPLink *)link)->isReady();
+                return (*(datalink_ptr *)link).link->isReady();
         }
 
         long next_message_size(void *link)
         {
-                auto ptr = ((TCPLink *)link);
+                auto ptr = (*(std::shared_ptr<Datalink> *)link);
                 return ptr->readMessageSize();
         }
 
         long read_next_message(void *link, uint8_t *buffer, long size, double *timestamp)
         {
-                auto ptr = ((TCPLink *)link);
+                auto ptr = (*(std::shared_ptr<Datalink> *)link);
                 return ptr->readMessageToBuffer(buffer, size, timestamp);
         }
 
         bool has_data(void *link)
         {
-                auto ptr = ((TCPLink *)link);
+                auto ptr = (*(std::shared_ptr<Datalink> *)link);
                 return ptr->hasData();
         }
 
         void clear_buffer(void *link)
         {
-                auto ptr = ((TCPLink *)link);
+                auto ptr = (*(std::shared_ptr<Datalink> *)link);
                 return ptr->clearBuffer();
         }
 
-        bool write_keep_alive_message(void *link) {
-                auto ptr = ((TCPLink *)link);
-                return ptr->writeKeepAlive();                
+        bool write_keep_alive_message(void *link)
+        {
+                auto ptr = (*(std::shared_ptr<Datalink> *)link);
+                return ptr->writeKeepAlive();
         }
 }
